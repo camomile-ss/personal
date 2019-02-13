9a602171aee845962bb46a907b29901da7a1aefb
otani <keiko.mf.b1204@gmail.com>
2019-02-13 14:27:32 +0900

    8

--- zero_nlp/chap1.py
+++ zero_nlp/chap1.py
@@ -26,7 +26,7 @@ def cross_entropy_error(y, t):
     if y.ndim == 2 and t.ndim == 1:  # 教師データがラベルだったらone-hotに
         t_ = np.zeros((t.size, y.shape[1]))
         t_[range(t.size), t] = 1
-        t = t_        
+        t = t_
     if y.ndim != 2 or t.ndim != 2:
         print('[err] dim ne 2. y:', y.ndim, ', t:', t.ndim, '. (cross_entropy_error)')
         sys.exit()
@@ -39,11 +39,11 @@ def cross_entropy_error_text(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
-        
+
     # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
     if t.size == y.size:
         t = t.argmax(axis=1)
-             
+
     batch_size = y.shape[0]
 
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
@@ -51,15 +51,38 @@ def cross_entropy_error_text(y, t):
 class Sigmoid:
     def __init__(self):
         self.params = []
-        
+        self.y = None
+
     def forward(self, x):
-        return sigmoid(x)
+        y = sigmoid(x)
+        self.y = y
+        return y
+
+    def backward(self, dy):
+        dx = self.y * (1 - self.y) * dy
+        return dx
+
+class Relu:
+    def __init__(self):
+        self.params = []
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)  # x<=0 -> True を保持
+        y = x
+        y[self.mask] = 0
+        return y
+
+    def backward(self, dy):
+        dx = dy
+        dx[self.mask] = 0
+        return dx
 
 class Affine:
     ''' 全結合層 '''
     def __init__(self, w, b):
         self.params = [w, b]
-        
+
     def forward(self, x):
         w, b = self.params
         return np.dot(x, w) + b
@@ -86,7 +109,7 @@ class SoftmaxWithLoss:
 class TwoLayersNet:
     def __init__(self, input_size, hidden_size, output_size):
         i, h, o = input_size, hidden_size, output_size
-        
+
         w1 = np.random.randn(i, h)
         b1 = np.random.randn(h)
         w2 = np.random.randn(h, o)
@@ -96,8 +119,8 @@ class TwoLayersNet:
             Affine(w1, b1),
             Sigmoid(),
             Affine(w2, b2)
-        ]        
-        
+        ]
+
         self.params = []
         for l in self.layers:
             self.params += l.params
@@ -110,6 +133,20 @@ class TwoLayersNet:
 if __name__=='__main__':
 
     x = np.random.randn(10,3)
+    print(x)
+
+    relu_layer = Relu()
+
+    y = relu_layer.forward(x)
+    print(y)
+
+    dy = np.ones((10,3))
+
+    dx = relu_layer.backward(dy)
+    print(dx)
+
+    """
+    x = np.random.randn(10,3)
     y = softmax(x)
     t_oh = np.array([0]*30).reshape(10,3)
     for i in range(10):
@@ -118,7 +155,7 @@ if __name__=='__main__':
     t = softmax(t)
 
     t_lbl = np.random.randint(0,3,10)
-    
+
     print('x:', x)
     print('y:', y)
     print('tがone hot:', t_oh)
@@ -134,4 +171,5 @@ if __name__=='__main__':
     #model = TwoLayersNet(2, 4, 3)
     #result = model.predict(x)
     #print(result)
-    
+    """
+
