b8fa486d119552ade21f1ddb1181e7c8f203e1ce
otani <keiko.mf.b1204@gmail.com>
2019-06-27 10:14:18 +0900

    0627

--- zero_nlp/negative_sampling_layers.py
+++ zero_nlp/negative_sampling_layers.py
@@ -4,21 +4,21 @@ from layers import Embedding
 
 class EmbeddingDot:
     def __init__(self, w):
-        self.emb = Embedding(w)
-        self.params = self.emb.params
-        self.grads = self.emb.grads
+        self.embed = Embedding(w)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
         self.cache = None
 
     def forward(self, h, idx):
-        w_i = self.emb.forward(idx)
-        s = np.sum(h * w_i, axis=1)
-        self.cache = (h, w_i)
+        w_idx = self.embed.forward(idx)
+        s = np.sum(h * w_idx, axis=1)
+        self.cache = (h, w_idx)
         return s
 
     def backward(self, ds):
         ds = ds.reshape(ds.shape[0], 1)  # ???
-        h, w_i = self.cache
-        dw_i = ds * h
-        self.emb.backward(dw_i)
-        dh = ds * w_i
+        h, w_idx = self.cache
+        dw_idx = ds * h
+        self.embed.backward(dw_idx)
+        dh = ds * w_idx
         return dh
