63d66bac7831f40e1c7bc97a3b06d32c350be39f
otani <keiko.mf.b1204@gmail.com>
2019-07-01 11:09:27 +0900

    0701

--- zero_nlp/negative_sampling_layers.py
+++ zero_nlp/negative_sampling_layers.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import numpy as np
+import collections
 from layers import Embedding
+from config import GPU
 
 class EmbeddingDot:
     def __init__(self, w):
@@ -22,3 +24,78 @@ class EmbeddingDot:
         self.embed.backward(dw_idx)
         dh = ds * w_idx
         return dh
+
+class UnigramSampler:
+    def __init__(self, corpus, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = corpus.max() + 1  # 単語数
+        # 確率分布 (x: word id)
+        self.word_p = np.array([np.count_nonzero(corpus==x) for x in range(self.vocab_size)])
+        self.word_p = np.power(self.word_p, power)
+        self.word_p /= np.sum(self.word_p)
+
+    def get_negative_sample(self, target):
+        n = target.shape[0]  # batch size
+
+        negative_sample = np.zeros((n, self.sample_size), dtype=np.int32)
+        for i in range(n):
+            p = self.word_p.copy()
+            p[target[i]] = 0  # targetの確率をゼロに
+            p /= np.sum(p)
+            negative_sample[i, :] = np.random.choice(self.vocab_size, size=self.sample_size, replace=False, p=p)
+
+        return negative_sample
+
+class UnigramSampler_text:
+    ''' 著者さま提供のほう '''
+    def __init__(self, corpus, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = None
+        self.word_p = None
+
+        counts = collections.Counter()
+        for word_id in corpus:
+            counts[word_id] += 1
+
+        vocab_size = len(counts)
+        self.vocab_size = vocab_size
+
+        self.word_p = np.zeros(vocab_size)
+        for i in range(vocab_size):
+            self.word_p[i] = counts[i]
+
+        self.word_p = np.power(self.word_p, power)
+        self.word_p /= np.sum(self.word_p)
+
+    def get_negative_sample(self, target):
+        batch_size = target.shape[0]
+
+        if not GPU:
+            negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
+
+            for i in range(batch_size):
+                p = self.word_p.copy()
+                target_idx = target[i]
+                p[target_idx] = 0
+                p /= p.sum()
+                negative_sample[i, :] = np.random.choice(self.vocab_size, size=self.sample_size, replace=False, p=p)
+        else:
+            # GPU(cupy）で計算するときは、速度を優先
+            # 負例にターゲットが含まれるケースがある
+            negative_sample = np.random.choice(self.vocab_size, size=(batch_size, self.sample_size),
+                                               replace=True, p=self.word_p)
+
+        return negative_sample
+
+if __name__ == '__main__':
+
+    corpus = np.array([0,1,2,3,4,2,3,5,0,1,2])
+    power=0.75
+    sample_size=2
+
+    sampler = UnigramSampler(corpus, power, sample_size)
+    sampler_text = UnigramSampler_text(corpus, power, sample_size)
+
+    print(sampler.word_p)
+    print(sampler_text.word_p)
+
