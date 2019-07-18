a45258d1c7726661302b3527b58dbdfb253bbee6
otani <keiko.mf.b1204@gmail.com>
2019-07-18 15:36:29 +0900

    combi

--- combinations.py
+++ combinations.py
@@ -1,22 +1,19 @@
 # coding: utf-8
-#from route_type import combi_rec
 
 def combinations(table):
     '''
-    リストのリストの、すべての組み合わせを返す。再帰処理。
-    e. g.) input:  [[1,2,3], [4,5], [6,7], [8]]
-           output: [[1, 4, 6, 8], [2, 4, 6, 8], [3, 4, 6, 8],
-                    [1, 5, 6, 8], [2, 5, 6, 8], [3, 5, 6, 8],
-                    [1, 4, 7, 8], [2, 4, 7, 8], [3, 4, 7, 8],
-                    [1, 5, 7, 8], [2, 5, 7, 8], [3, 5, 7, 8]]
+    リストのリストの、すべての組み合わせを返す再帰処理。
+    e.g.) input:  [[1,2,3], [4,5], [6,7], [8]]
+          output: [[1, 4, 6, 8], [1, 4, 7, 8], [1, 5, 6, 8], [1, 5, 7, 8],
+                   [2, 4, 6, 8], [2, 4, 7, 8], [2, 5, 6, 8], [2, 5, 7, 8],
+                   [3, 4, 6, 8], [3, 4, 7, 8], [3, 5, 6, 8], [3, 5, 7, 8]]
     '''
     if len(table)==1:
         return [[x] for x in table[0]]
     combis = []
-    for a in table[-1]:
-        for c in combinations(table[:-1]):
-            c.append(a)
-            combis.append(c)
+    for c in combinations(table[:-1]):
+        for a in table[-1]:
+            combis.append(c + [a])
     return combis
 
 if __name__ == '__main__':
