5dfcbc97f1cad2fcd6fc8fd3c044aa0652c1be84
otani <keiko.mf.b1204@gmail.com>
2019-07-19 12:01:00 +0900

    rnn

--- zero_nlp/time_layers.py
+++ zero_nlp/time_layers.py
@@ -1,15 +1,29 @@
 # coding: utf-8
 import numpy as np
+from layers import MatMul
 
 class RNN:
     def __init__(self, wh, wx, b):
-        self.params = [wh, wx, b]
-        self.grads = [np.zeros_like(wh), np.zeros_like(wx), np.zeros_like(b)]
-        self.cache = None
+        self.matmul_h = MatMul(wh)
+        self.matmul_x = MatMul(wx)
+        self.params = self.matmul_h.params + self.matmul_x.params + [b]
+        self.grads = self.matmul_h.grads + self.matmul_x.grads + [np.zeros_like(b)]
+        self.h = None
 
     def forward(self, x, h_prev):
-        wh, wx, b = self.params
-        t = np.dot(h_prev, wh) + np.dot(x, wx) + b
+        b = self.params[2]
+        t = self.matmul_h.forward(h_prev) + self.matmul_x.forward(x) + b
         h = np.tanh(t)
-        self.cache = [x, h_prev, h]
+        self.h = h
         return h
+
+    def backward(self, dh):
+        dt = dh * (1 - self.h ** 2)
+        db = np.sum(dt, axis=0)
+
+        dh_prev = self.matmul_h.backward(dt)
+        dx = self.matmul_x.badkward(dt)
+
+        self.grads[2][...] = db
+
+        return dx, dh_prev
